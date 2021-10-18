2a42957ba6ef4f7e144a8cc333ec272543697dc1
Vlad Ivolgin <87894035+vladivolgin@users.noreply.github.com>
2021-10-18 21:50:42 +0600

    Add files via upload

---
+++ Bank/Bank/Account.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// тип счета
+    /// </summary>
+    enum BalanceType
+    {
+        credit,
+        debit
+    }
+    class Account
+    {
+        private static int startId = 0;
+        private int id;
+        private decimal balance;
+        private BalanceType accountType;
+
+        
+        /// номер счета
+        
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+
+        
+        /// баланс
+        
+        public decimal Balance
+        {
+            get
+            {
+                return this.balance;
+            }
+        }
+
+        
+        /// тип счета
+        
+        public BalanceType AccountType
+        {
+            get
+            {
+                return this.accountType;
+            }
+        }
+
+        public Account() : this(BalanceType.debit, 0) { }
+
+        public Account(BalanceType type) : this(type, 0) { }
+
+        public Account(decimal balance) : this(BalanceType.debit, balance) { }
+
+
+        public Account(BalanceType type, decimal balance)
+        {
+            if (StartBalanceValidation(balance))
+            {
+                this.balance = balance;
+            }
+            else
+            {
+                this.balance = 0;
+            }
+
+            this.accountType = type;
+            this.id = CreateAcountNumber();
+
+        }
+
+        private int CreateAcountNumber()
+        {
+            startId = startId + 1;
+            return startId;
+        }
+
+        public bool Added(decimal add)
+        {
+            if (!CanAdd(add)) { return false; }
+
+            this.balance = this.balance + add;
+
+            return true;
+        }
+
+        public bool Withdrawed(decimal withdraw)
+        {
+            if (!CanWithdraw(withdraw)) { return false; }
+
+            this.balance = this.balance - withdraw;
+
+            return true;
+        }
+
+        //проверка что денежные средства можно положить на счет
+        //проверяется корректность введенной суммы, лимиты и т.д.
+        private bool CanAdd(decimal add)
+        {
+            if (add < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        //проверка что дс можно снять со счета
+        //проверяется корректность введенной суммы, лимиты и т.д.
+        private bool CanWithdraw(decimal withdraw)
+        {
+            if (withdraw < 0)
+            {
+                return false;
+            }
+            if ((this.balance - withdraw) < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        //проверка что стартовый баланс при создании класса соответствует требованиям
+        private bool StartBalanceValidation(decimal balance)
+        {
+            if (balance < 0) { return false; }
+
+            return true;
+        }
+
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,45 @@
+﻿using BankAccount;
+using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account ac1 = new Account();
+            Account ac2 = new Account();
+
+            Console.WriteLine($"{ac1.Id} {ac2.Id}");
+            Console.WriteLine($"{ac1.Balance} {ac2.Balance}");
+            Console.WriteLine($"{ac1.AccountType} {ac2.AccountType}");
+
+            Account ac3 = new Account(1000);
+
+            Console.WriteLine($"{ac3.Id} {ac3.AccountType} {ac3.Balance} баланс должен быть 1000");
+
+            ac3.Withdrawed(1500);
+
+            Console.WriteLine($"{ac3.Id} {ac3.AccountType} {ac3.Balance} баланс должен быть 1000");
+
+            ac3.Added(2000);
+
+            Console.WriteLine($"{ac3.Id} {ac3.AccountType} {ac3.Balance} баланс должен быть 3000");
+
+            ac3.Withdrawed(1000);
+
+            Console.WriteLine($"{ac3.Id} {ac3.AccountType} {ac3.Balance} баланс должен быть 2000");
+
+            ac3.Withdrawed(-1000);
+
+            Console.WriteLine($"{ac3.Id} {ac3.AccountType} {ac3.Balance} баланс должен быть 2000");
+
+            Account ac4 = new Account(-100);
+
+            Console.WriteLine($"{ac4.Id} {ac4.AccountType} {ac4.Balance} баланс должен быть 0");
+
+
+            Console.ReadLine();
+        }
+    }
+}
